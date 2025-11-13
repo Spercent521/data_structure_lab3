26b4be07af83c3cd5db10de022f3237c26884086
Spercent521_231_ubuntu <439713280@qq.com>
2025-11-13 16:13:53 +0800

    feature:bst complete

--- src/sort_method/bst.rs
+++ src/sort_method/bst.rs
@@ -28,20 +28,68 @@ impl BST {
 
     // insert value
     pub fn insert(&mut self, val: i32, origin_pos: i32) {
-        todo!("实现将值和其原始位置插入二叉排序树的逻辑");
+        let new_node = Box::new(TreeNode::new(val, origin_pos));
+        if self.root.is_none() {
+            self.root = Some(new_node);
+            return;
+        }
+
+        let mut current = &mut self.root;
+        loop {
+            match current {
+                Some(node) => {
+                    if val < node.val {
+                        if node.left.is_none() {
+                            node.left = Some(new_node);
+                            return;
+                        } else {
+                            current = &mut node.left;
+                        }
+                    } else {
+                        if node.right.is_none() {
+                            node.right = Some(new_node);
+                            return;
+                        } else {
+                            current = &mut node.right;
+                        }
+                    }
+                },
+                None => return,
+            }
+        }
     }
 
     // search value return its original position in sequence(BST->pos_in_sequence)
     pub fn search(&self, val: i32) -> Option<i32> {
-        todo!("实现在二叉排序树中查找值的逻辑，如果找不到则返回 None");
+        if self.root.is_none() {
+            return None;
+        }
+        let mut current = &self.root;
+        loop{
+            match current {
+                Some(node) => {
+                    if val == node.val {
+                        return Some(node.origin_pos_in_sequence);
+                    } else if val < node.val {
+                        current = &node.left;
+                    } else {
+                        current = &node.right;
+                    }
+                },
+                None => return None,
+            }
+        }
     }
 }
 
 /// 从序列构建一个二叉排序树
 pub fn build_bst(sequence: &[i32]) -> BST {
+    // 按理说从序列中构建BST应该先排序再插入 以保证树的平衡 但这里我们直接插入原始序列以符合题意
     let mut bst = BST::new();
     
-    todo!("从序列构建二叉排序树");
+    for (index, &value) in sequence.iter().enumerate() {
+        bst.insert(value, index as i32);
+    }
 
     bst
 }
@@ -50,11 +98,11 @@ pub fn build_bst(sequence: &[i32]) -> BST {
 pub fn search_n_bst(sequence: &[i32], test_case_for_search: &[i32]) -> Vec<usize> {
     println!("{}", "使用二叉排序树查找多个目标元素...".bright_black());
 
-    let start = Instant::now();
-
     let mut result = Vec::new();
 
     let bst = build_bst(sequence);
+
+    let start = Instant::now();
 
     for &target in test_case_for_search {
         // 假设未找到时，我们添加一个特殊值（例如 usize::MAX）或直接忽略
